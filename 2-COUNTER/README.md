f195d70b328b4a461839e4a15e771ba41504d913
A-Jha <avvinashjha@gmail.com>
2021-05-25 06:18:39 +0530

    Day-2 JS-Mini-Project-2 completed
    Now playing with time is next task :)

---
+++ 2-COUNTER/README.md
@@ -0,0 +1,5 @@
+# Counter
+
+The basic idea is to targer different elements on one shot and then basec on the class change the content.
+
+## We will add stop watch function soon
---
+++ 2-COUNTER/app.js
@@ -0,0 +1,30 @@
+//set count value to zero
+//we are not using const because const can not change
+let count = 0;
+
+const value = document.querySelector("#value");
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("btnDecrease")) {
+      count--;
+    } else if (styles.contains("btnIncrease")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
+});
