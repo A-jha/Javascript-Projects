e68022b4060b12d8b900217e95f52856c2cfe617
A-Jha <avvinashjha@gmail.com>
2021-06-18 10:45:20 +0530

    Added math file

---
+++ 16-MATH/script.js
@@ -0,0 +1,16 @@
+//get each element of matrix1
+const el1_00 = document.getElementById("m100").value;
+const el1_01 = document.getElementById("m101").value;
+const el1_02 = document.getElementById("m102").value;
+const el1_10 = document.getElementById("m110").value;
+const el1_11 = document.getElementById("m111").value;
+const el1_12 = document.getElementById("m112").value;
+
+console.log(el2_01);
+//get each element of matrix2
+const el2_00 = document.getElementById("m200").value;
+const el2_01 = document.getElementById("m201").value;
+const el2_10 = document.getElementById("m210").value;
+const el2_11 = document.getElementById("m211").value;
+const el2_20 = document.getElementById("m220").value;
+const el2_21 = document.getElementById("m221").value;
